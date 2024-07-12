e1e49dfa8ae5c39dae0edb4e858cc4aea39d6110
JC-Dulos10 <carlosdulos@gmail.com>
2024-07-12 12:53:24 +1000

    Update page.tsx

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -126,7 +126,7 @@ const ProductPage = () => {
           </a>
           <a href='/dashboard'>
             <button className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-red-700">
-              back
+              Back
             </button>
           </a>
           <CreateProductModal
