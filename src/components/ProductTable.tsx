798b37eadf675f06e469dc898e12841eb7ab0b9b
JC-Dulos10 <carlosdulos@gmail.com>
2025-01-21 15:13:54 +1100

    updated product table
    added status, creator ID, category

--- src/components/ProductTable.tsx
+++ src/components/ProductTable.tsx
@@ -5,6 +5,9 @@ interface Product {
   productId: number;
   productName: string;
   productPrice: number;
+  category: {categoryName: String};
+  status: string;
+  creatorUserId: number;
 }
 
 interface Props {
@@ -42,6 +45,15 @@ const ProductTable: React.FC<Props> = ({ products, isAdmin, onDelete }) => {
             <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
               Price
             </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Category
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Status
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Created by
+            </th>
             {isAdmin && (
               <>
                 <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
@@ -65,6 +77,15 @@ const ProductTable: React.FC<Props> = ({ products, isAdmin, onDelete }) => {
               </td>
               <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                 {product.productPrice}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {product.category.categoryName}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {product.status}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                {product.creatorUserId}
               </td>
               {isAdmin && (
                 <>
